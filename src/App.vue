cba2915ed8d025b6d1f7db38ca6bad5b997bbcac
DanieleBegnis <daniele.begnis@gmail.com>
2024-06-10 16:26:59 +0200

    created frontoffice, added bootstrap, fixed app.vue and projectslist

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script>
-export default {}
+import ProjectsList from './components/ProjectsList.vue';
+export default {
+  components: {
+    ProjectsList
+  }
+}
 </script>
 
 <template>
   test test test
+  <h1>test</h1>
+  <ProjectsList></ProjectsList>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectsList.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: 'ProjectsList'
+}
+</script>
+
+<template>
+    test lista
+</template>
+
+<style scoped lang="scss">
+</style>
